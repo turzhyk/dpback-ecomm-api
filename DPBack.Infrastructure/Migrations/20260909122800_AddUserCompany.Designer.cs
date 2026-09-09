7467afc8ed6911146042c2007eef14eb311a4e05
turzhyk <andrewurbro@yahoo.com>
2026-09-09 14:28:21 +0200

    fix uncorrect migration

--- DPBack.Infrastructure/Migrations/20260909122800_AddUserCompany.Designer.cs
+++ DPBack.Infrastructure/Migrations/20260909122800_AddUserCompany.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DPBack.Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DPBack.Infrastructure.Migrations
 {
     [DbContext(typeof(OrderStoreDbContext))]
-    partial class OrderStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260909122800_AddUserCompany")]
+    partial class AddUserCompany
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DPBack.Infrastructure/Migrations/20260909122800_AddUserCompany.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DPBack.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserCompany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyName",
+                table: "Customers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nip",
+                table: "Customers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Regon",
+                table: "Customers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompanyName",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Nip",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Regon",
+                table: "Customers");
+        }
+    }
+}
