cb20e5b1e6e4020b99363d3aa6820a564b79dc16
Turzhyk <andrewurbro@yahoo.com>
2026-09-21 07:46:11 +0200

    refactor: change customer model

--- DPBack.Domain/Models/Customer.cs
+++ DPBack.Domain/Models/Customer.cs
@@ -4,11 +4,26 @@ public class Customer
 {
     public Guid Id { get; set; }
     public required string Name { get; set; }
-    public required string Phone { get; set; }
+    public required string Phone { get; init; }
     public string? Email { get; set; }
     public List<CustomerAddress>? Addresses { get; set; }
     public Guid? UserId { get; set; }
     public string? Nip { get; set; }
     public string? Regon { get; set; }
     public string? CompanyName { get; set; }
+
+    public bool Equals(Customer customer)
+    {
+        return customer.Phone == Phone;
+    }
+
+    public override bool Equals(object? obj)
+     {
+        return Equals(obj as Customer);
+    }
+
+    public override int GetHashCode()
+    {
+        return Phone.GetHashCode();
+    }
 }
